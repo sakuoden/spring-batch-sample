58583d26aaae377a788a60a13b296fe72e6925ce
sakuoden <4oden18@gmail.com>
2022-06-25 15:07:50 +0900

    first commit

---
+++ src/test/java/jtn/springbatchsample/SpringBatchSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package jtn.springbatchsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
